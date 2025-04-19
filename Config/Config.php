c9102a9a6a7fd402207e8e787f038aa378916691
pro3979375953 <juanramires33564@gmail.com>
2025-04-18 23:06:32 -0500

    Add files via upload

---
+++ Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+const BASE_URL = "http://localhost/tienda-online/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda-online";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "USD";
+const CLIENT_ID = "AaYHjbeyXnYTNjiXA66ka6P3LVhYxxh6B_k4zKNyrVYNMrLLgIM9p3OD3ILW8FpZTY1dOktfM0oaEiCR";
+
+const USER_SMTP = "proyectodemejora28@gmail.com";
+const PASS_SMTP = "gihe ffuj epjc cvrj";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
